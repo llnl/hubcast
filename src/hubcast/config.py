a3302c820f79f45e036d32154ae29a06253468fa
Alec Scott <alec@llnl.gov>
2026-03-03 16:40:28 -0800

    Set REQUESTER global in Hubcast __main__
    Signed-off-by: Alec Scott <alec@llnl.gov>

--- src/hubcast/config.py
+++ src/hubcast/config.py
@@ -1,11 +1,6 @@
 import os
 from typing import Optional
 
-# for both github and gitlab `requester`
-# does not perform any function other than
-# identifying the client in user-agent headers
-REQUESTER = "hubcast"
-
 
 class ConfigError(Exception):
     pass
@@ -39,7 +34,6 @@ class GitHubConfig:
     def __init__(self):
         self.app_id = env_get("HC_GH_APP_IDENTIFIER")
         self.privkey = env_get("HC_GH_PRIVATE_KEY")
-        self.requester = REQUESTER
         self.webhook_secret = env_get("HC_GH_SECRET")
         self.bot_user = env_get("HC_GH_BOT_USER")
 
@@ -47,7 +41,6 @@ class GitHubConfig:
 class GitLabConfig:
     def __init__(self):
         self.instance_url = env_get("HC_GL_URL")
-        self.requester = REQUESTER
         self.token = env_get("HC_GL_TOKEN")
         self.token_type = env_get("HC_GL_TOKEN_TYPE", default="impersonation")
         self.webhook_secret = env_get("HC_GL_SECRET")
