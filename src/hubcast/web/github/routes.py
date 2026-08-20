a4449e4f09b815fd159ea674dda7305e65b3db55
Alec Scott <alec@llnl.gov>
2026-08-19 18:44:36 -0700

    Mirror tags and branches with already mirrored refs (#411)
    Upstream fix of https://github.com/llnl/repligit/pull/216
    
    ---------
    
    Signed-off-by: Alec Scott <alec@llnl.gov>

--- src/hubcast/web/github/routes.py
+++ src/hubcast/web/github/routes.py
@@ -182,7 +182,10 @@ async def sync_branch(
 
     update_log_context(from_sha=from_sha, want_sha=want_sha)
 
-    if want_sha in have_shas:
+    # directly check from_sha equals want_sha for cases where the sha has
+    # already been mirrored but the ref is out-of-date. This is commonly the
+    # case for tags that are created against an existing commit on a branch.
+    if from_sha == want_sha:
         log.info("Skipped branch sync - already up-to-date")
         return
 
@@ -393,7 +396,10 @@ async def sync_pr(
     from_sha = gl_refs.get(sync_ref) or ("0" * 40)
     update_log_context(from_sha=from_sha, want_sha=want_sha)
 
-    if want_sha in have_shas:
+    # directly check from_sha equals want_sha for cases where the sha has
+    # already been mirrored but the ref is out-of-date. This is commonly the
+    # case for tags that are created against an existing commit on a branch.
+    if from_sha == want_sha:
         log.info("Skipped PR sync - already up-to-date")
     else:  # needs sync
         if is_pull_request_fork and not src_repo_private:
